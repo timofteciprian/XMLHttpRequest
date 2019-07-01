e71c5383066a2b57da1207dc9a6b9403cc2694a6
Unknown <timofteca@gmail.com>
2019-07-01 13:58:10 +0300

    added collapse

--- components/films/Films.js
+++ components/films/Films.js
@@ -4,113 +4,117 @@ function retriveData(url, callback) {
   request.open("GET", url);
   request.onload = function() {
     var data = JSON.parse(this.response);
-    console.log(data);
     callback(data);
   };
   request.send();
 }
 
-// function displayData() {
-//   url = "https://swapi.co/api/films";
-//   data = returnData(url);
-// }
-
 function loadXMLDoc() {
-  det = retriveData();
-  console.log(det);
-  var request = new XMLHttpRequest();
-  request.responseType = "text";
-  request.open("GET", "https://swapi.co/api/films");
-  request.onload = function() {
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-      data.results.map((element, index) => {
-        var li = document.createElement("li");
-        li.setAttribute("class", "list-group-item");
+  var url = "https://swapi.co/api/films";
+  retriveData(url, function(data) {
+    data.results.map((element, index) => {
+      var div = document.createElement("div");
+      div.setAttribute("class", "accordin");
+      div.setAttribute("id", "accordionExample");
 
-        var anchorElmnt = document.createElement("Button");
-        anchorElmnt.setAttribute("type", "button");
-        anchorElmnt.setAttribute("class", "btn btn-primary");
-        anchorElmnt.setAttribute("data-toggle", "modal");
-        anchorElmnt.setAttribute("data-target", ".bd-example-modal-lg");
-        anchorElmnt.innerHTML = element.title;
-        li.appendChild(anchorElmnt);
-        document.getElementById("container").appendChild(li);
+      var div1 = document.createElement("div");
+      div1.setAttribute("class", "card");
+      div.appendChild(div1);
 
-        var title = document.createElement("p");
-        title.setAttribute("class", "title");
-        title.innerHTML = element.title;
-        document.getElementById("title").appendChild(title);
+      var div2 = document.createElement("div");
+      div2.setAttribute("class", "card-header");
+      div2.setAttribute("id", "headingOne");
+      div1.appendChild(div2);
 
-        document.getElementById("episode_id").innerHTML = element.episode_id;
-        document.getElementById("opening_crawl").innerHTML =
-          element.opening_crawl;
-        document.getElementById("director").innerHTML = element.director;
-        document.getElementById("producer").innerHTML = element.producer;
-        document.getElementById("release_date").innerHTML =
-          element.release_date;
-        document.getElementById("characters").innerHTML = element.characters;
-      });
+      var h2 = document.createElement("h2");
+      h2.setAttribute("class", "mb-0");
+      div2.appendChild(h2);
 
-      // $(document).on("click", ".open-homeEvents", function() {
-      //   var eventId = $(this).data("id");
-      //   $("#idHolder").html(eventId);
-      // });
-      // $(".container").html(html);
-    }
-  };
-  request.send();
+      var btn = document.createElement("Button");
+      btn.setAttribute("class", "btn btn-link");
+      btn.setAttribute("type", "button");
+      btn.setAttribute("data-toggle", "collapse");
+      btn.setAttribute("data-target", "#collapseOne");
+      btn.setAttribute("aria-expanded", "true");
+      btn.setAttribute("aria-controls", "collapseOne");
+      btn.innerHTML = element.title;
+      h2.appendChild(btn);
+
+      var div3 = document.createElement("div");
+      div3.setAttribute("class", "collapse show");
+      div3.setAttribute("id", "collapseOne");
+      div3.setAttribute("aria-labelledby", "headingOne");
+      div3.setAttribute("data-parent", "#accordionExample");
+      div1.appendChild(div3);
+
+      var div4 = document.createElement("div");
+      div4.setAttribute("class", "card-body");
+      div3.appendChild(div4);
+
+      var title = document.createElement("p");
+      title.innerHTML = element.title;
+      div4.appendChild(title);
+
+      var opening_crawl = document.createElement("p");
+      opening_crawl.innerHTML = element.opening_crawl;
+      div4.appendChild(opening_crawl);
+
+      document.getElementById("container").appendChild(div);
+    });
+  });
 }
 
 // function loadXMLDoc() {
-//   var request = new XMLHttpRequest();
-//   request.responseType = "text";
-//   request.open("GET", "https://swapi.co/api/films");
-//   request.onload = function() {
-//     var data = JSON.parse(this.response);
-//     if (request.status >= 200 && request.status < 400) {
-//       var html = "";
-//       $.each(data.results, function(key, element) {
-//         html +=
-//           '<li class="list-group-item">' +
-//           '<button type="button" class="open-homeEvents btn btn-primary" data-id="' +
-//           "Film: " +
-//           element.title +
-//           " " +
-//           element.episode_id +
-//           " " +
-//           element.opening_crawl +
-//           '" data-toggle="modal" data-target=".bd-example-modal-xl" href="#addBookDialog">' +
-//           element.title +
-//           "</button>" +
-//           "</li>";
-//       });
+//   var url = "https://swapi.co/api/films";
+//   retriveData(url, function(data) {
+//     // state = data;
+//     // console.log("state: ", state);
 
-//       $(document).on("click", ".open-homeEvents", function() {
-//         var eventId = $(this).data("id");
-//         $("#idHolder").html(eventId);
-//       });
-//       $(".container").html(html);
-//     }
-//   };
-//   request.send();
-// }
+//     data.results.map((element, index) => {
+//       var li = document.createElement("li");
+//       li.setAttribute("class", "list-group-item");
 
-// function loadXMLDoc() {
-//   var request = new XMLHttpRequest();
-//   request.responseType = "text";
-//   request.open("GET", "https://swapi.co/api/films/1");
-//   request.onload = function() {
-//     var data = JSON.parse(this.response);
-//     if (request.status >= 200 && request.status < 400) {
-//       document.getElementById("container").innerHTML = data.title;
-//       document.getElementById("episode_id").innerHTML = data.episode_id;
-//       document.getElementById("opening_crawl").innerHTML = data.opening_crawl;
-//       document.getElementById("director").innerHTML = data.director;
-//       document.getElementById("producer").innerHTML = data.producer;
-//       document.getElementById("release_date").innerHTML = data.release_date;
-//       document.getElementById("characters").innerHTML = data.characters;
-//     }
-//   };
-//   request.send();
+//       var btn = document.createElement("Button");
+//       //btn.setAttribute("onclick", "populateModal()");
+//       btn.setAttribute("type", "button");
+//       btn.setAttribute("class", "btn btn-primary");
+//       btn.setAttribute("data-toggle", "modal");
+//       btn.setAttribute("data-target", ".bd-example-modal-lg");
+//       //btn.setAttribute("data-id", element.episode_id);
+//       btn.innerHTML = element.title;
+//       li.appendChild(btn);
+//       document.getElementById("container").appendChild(li);
+
+//       var div1 = document.createElement("div");
+//       div1.setAttribute("class", "modal fade bd-example-modal-lg");
+//       div1.setAttribute("tabindex", "-1");
+//       div1.setAttribute("role", "dialog");
+//       div1.setAttribute("aria-labelledby", "myLargeModalLabel");
+//       div1.setAttribute("aria-hidden", "true");
+
+//       var div2 = document.createElement("div");
+//       div2.setAttribute("class", "modal-dialog modal-lg");
+//       div1.appendChild(div2);
+
+//       var div3 = document.createElement("div");
+//       div3.setAttribute("class", "modal-content");
+//       div2.appendChild(div3);
+
+//       var title = document.createElement("p");
+//       title.setAttribute("class", "title");
+//       title.innerHTML = element.title;
+//       div3.appendChild(title);
+//       console.log(div1);
+//         document.getElementById("episode_id").innerHTML = element.episode_id;
+//         document.getElementById("opening_crawl").innerHTML =
+//           element.opening_crawl;
+//         document.getElementById("director").innerHTML = element.director;
+//         document.getElementById("producer").innerHTML = element.producer;
+//         document.getElementById("release_date").innerHTML =
+//           element.release_date;
+//         document.getElementById("characters").innerHTML = element.characters;
+
+//       document.getElementById("containerr").appendChild(div1);
+//     });
+//   });
 // }
